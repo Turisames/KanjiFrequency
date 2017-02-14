f5f2b4c74b043068465cf2f9577eff32a6e06cc1
Turisames <turisames@gmail.com>
2017-02-14 16:24:31 +0200

    Added implementation to the GUI file. In time, I will make it prettier.
    Started making a file dialogue function. It will need more work.

--- GUI.py
+++ GUI.py
@@ -1,6 +1,26 @@
 import tkinter as tk
+import tkinter.filedialog as filedialog
 
-
-class mainWindow(self):
+class mainWindow:
     def __init__(self):
-        pass
+        # Start the Window
+        self.__root = tk.Tk()
+
+        self.__root.title( "Kanji Frequency" )
+        #self.__root.iconbitmap("")
+
+        self.__takeFileButton = tk.Button( self.__root, text="Choose a text file full of Japanese texts.", command=self.__takeFile__  )
+        self.__takeFileButton.pack()
+
+        # Get the GUI going.
+        self.__root.mainloop()
+
+    def __takeFile__(self):
+        kanjiFile = filedialog.askopenfilename( filetypes=[ ("text files", ".txt") ] )
+
+        # If there was some trouble getting the file, cancel
+        if not kanjiFile or kanjiFile == "":
+                return
+
+
+mainWindow()
