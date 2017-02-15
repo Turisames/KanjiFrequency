14528057dfe523e1bfbc48c50ee7e6b215c57685
Turisames <turisames@gmail.com>
2017-02-15 18:44:20 +0200

    Added comments to the init() method of the GUI. This is to improve readability of the code and to help myself continue working on it in the future.

--- GUI.py
+++ GUI.py
@@ -12,16 +12,21 @@ class mainWindow:
         # Start the Window
         self.__root = tk.Tk()
 
+        # Core
         # Give this widget an instance of the KanjiCounter class.
         self.__core = core.KanjiCounter()
 
+        # Name and icon.
         self.__root.title( "Kanji Frequency" )
         #self.__root.iconbitmap("")
 
+        # The button for taking a file.
         self.__takeFileButton = tk.Button( self.__root, text="Choose a text file full of Japanese texts." , \
                                            command=self.__takeFile__ , padx=10, pady=10 )
         self.__takeFileButton.pack()
+        # </filebutton>
 
+        # The radiobuttons.
         self.__frequencyMode = FrequenceMode.REAL_NUMBERS
         self.__RealRadioBtn = tk.Radiobutton( self.__root, text="Real Numbers", \
                              variable=self.__frequencyMode, value=FrequenceMode.REAL_NUMBERS)
@@ -29,10 +34,13 @@ class mainWindow:
         self.__RelativeRadioBtn =tk.Radiobutton( self.__root, text="Relative Frequency", \
                                  variable=self.__frequencyMode, value=FrequenceMode.RELATIVE_NUMBER)
         self.__RelativeRadioBtn.pack()
+        # </radiobuttons>
 
+        # The widget for showing the results.
         self.__textWidget = tk.Text( self.__root, \
                                      padx=4, pady=4, width=30 )
         self.__textWidget.pack()
+        #</text>
 
         # Get the GUI going.
         self.__root.mainloop()
